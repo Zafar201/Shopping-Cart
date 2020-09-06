bcceb7a933cd67cda9b9245d807db95ecb3e693d
Zafar Irshad <zafarirshad201@gmail.com>
2020-09-06 19:32:49 +0530

    1;35

---
+++ Users/AXE/Desktop/NodejsZoomClone/public/script.js
@@ -0,0 +1,39 @@
+const socket = io('/')
+const videoGrid =document.getElementById('video-grid');
+console.log(videoGrid)
+const myVideo =document.createElement('video');
+myVideo.muted= true;
+
+var peer = new Peer(undefined,{
+    path :'/peerjs',
+    host: '/',
+    port: '3030'
+});
+
+let myVideoStream
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true
+}).then(stream =>{
+   myVideoStream = stream;
+   addVideoStream(myVideo,stream);
+})
+peer.on('open',id =>{
+    socket.emit('join-room',ROOM_ID,id)
+})
+
+
+socket.on('user-connected',(userId)=>{
+    connectToNewUser(userId)
+})
+const connectToNewUser =(userId)=>{
+    console.log(userId)
+}
+
+const addVideoStream =(video,stream) => {
+    video.srcObject = stream;
+    video,addEventListener('loadedmetadata',()=>{
+        video.play();
+    })
+    videoGrid.append(video);
+}
